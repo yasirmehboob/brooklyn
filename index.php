bcf1af884f0df410457e0265491470ed4caf8dbf
GhulamIshaq <ghulamishaq002@gmail.com>
2023-03-01 17:36:51 +0500

    Title Changed

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <meta name="description" content="Medcity - Medical Healthcare HTML5 Template">
   <link href="assets/images/favicon/favicon.png" rel="icon">
-  <title>Medcity - Medical Healthcare HTML5 Template</title>
+  <title>Brooklyn Pharmaceuticals</title>
 
   <link rel="stylesheet"
     href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&family=Roboto:wght@400;700&display=swap">
@@ -29,7 +29,7 @@
   
 
 
-  
+
     <!-- ============================
         Slider
     ============================== -->

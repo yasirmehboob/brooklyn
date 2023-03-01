1337fae96025748a838403a4e21611a1af677823
GhulamIshaq <ghulamishaq002@gmail.com>
2023-03-01 18:20:55 +0500

    About Title Changed

--- about-us.php
+++ about-us.php
@@ -5,9 +5,8 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <meta name="description" content="Medcity - Medical Healthcare HTML5 Template">
-  <link href="assets/images/favicon/favicon.png" rel="icon">
-  <title>Medcity - Medical Healthcare HTML5 Template</title>
+  <meta name="description" content="Brooklyn Pharmaceuticals">
+  <title>Brooklyn Pharmaceuticals</title>
 
   <link rel="stylesheet"
     href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&family=Roboto:wght@400;700&display=swap">

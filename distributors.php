b48a7626def9f94e3617ec0b5c9fe2e079edede7
BigBook Cloud <yasir_u@yahoo.com>
2023-05-02 18:37:28 +0500

    new updates
    new upates

---
+++ distributors.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <meta name="description" content="Brooklyn Pharmaceuticals">
+  <title>Brooklyn Pharmaceuticals | Distributors</title>
+  <link rel="shortcut icon" href="assets\images\favicon\favicon.png" type="image/x-icon">
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&family=Roboto:wght@400;700&display=swap">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
+  <link rel="stylesheet" href="assets/css/libraries.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+  <div class="wrapper">
+    <div class="preloader">
+      <div class="loading"><span></span><span></span><span></span><span></span></div>
+    </div><!-- /.preloader -->
+
+    
+    <?php
+     include('header.inc');
+    ?>
+
+    <!-- ========================
+       page title 
+    =========================== -->
+    <section class="page-title page-title-layout5">
+      <div class="bg-img"><img src="assets/images/backgrounds/6.jpg" alt="background"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 d-flex justify-content-between flex-wrap align-items-center">
+            <h1 class="pagetitle__heading my-3">Distributors Network</h1>
+            <nav>
+              <ol class="breadcrumb my-3">
+                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                <li class="breadcrumb-item"><a href="distributors.php">Distributors</a></li>
+              </ol>
+            </nav>
+          </div><!-- /.col-12 -->
+        </div><!-- /.row -->
+      </div><!-- /.container -->
+    </section><!-- /.page-title -->
+
+    <!-- ======================
+       FAQ
+    ========================= -->
+    <section class="faq pt-120 pb-70">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-12 col-lg-4">
+            <aside class="sidebar has-marign-right">
+              <div class="widget widget-help bg-overlay bg-overlay-secondary-gradient">
+                <div class="bg-img"><img src="assets/images/banners/5.jpg" alt="background"></div>
+                <div class="widget-content">
+                  <div class="widget__icon">
+                    <i class="icon-call3"></i>
+                  </div>
+                  <h4 class="widget__title">Emergency Case</h4>
+                  <p class="widget__desc">Please feel welcome to contact our friendly reception staff. With any general
+                   enquiry call us.
+                  </p>
+                  <a href="tel:(051) 4949539" class="phone__number">
+                    <i class="icon-phone"></i> <span>(051) 4949538-9</span>
+                  </a>
+                </div><!-- /.widget-content -->
+              </div><!-- /.widget-help -->
+              <div class="widget widget-schedule">
+                <div class="widget-content">
+                  <div class="widget__icon">
+                    <i class="icon-charity2"></i>
+                  </div>
+                  <h4 class="widget__title">Working Hours</h4>
+                  <ul class="time__list list-unstyled mb-0">
+                    <li><span>Monday - Saturday</span><span>9.00 am - 4:30 pm</span></li>
+                  </ul>
+                </div><!-- /.widget-content -->
+              </div><!-- /.widget-schedule -->
+            
+            </aside><!-- /.sidebar -->
+          </div><!-- /.col-lg-4 -->
+          
+            
+            <div class="col-sm-12 col-md-12 col-lg-8 fancybox-layout2">
+              <div class="row">
+                <div class="col-sm-6">
+                  <!-- fancybox item #1 -->
+                  <div class="fancybox-item d-flex">
+                    <div class="fancybox__icon">
+                      <i class="icon-diploma"></i>
+                    </div><!-- /.fancybox__icon -->
+                    <div class="fancybox__content">
+                      <h4 class="fancybox__title">Edison Awards</h4>
+                      <p class="fancybox__desc">Honoring excellence in innovation</p>
+                    </div><!-- /.fancybox-content -->
+                  </div><!-- /.fancybox-item -->
+                </div><!-- /.col-sm-6 -->
+                <div class="col-sm-6">
+                  <!-- fancybox item #2 -->
+                  <div class="fancybox-item d-flex">
+                    <div class="fancybox__icon">
+                      <i class="icon-diploma"></i>
+                    </div><!-- /.fancybox__icon -->
+                    <div class="fancybox__content">
+                      <h4 class="fancybox__title">Edwin Grant Medal</h4>
+                      <p class="fancybox__desc">Research in developmental biology</p>
+                    </div><!-- /.fancybox-content -->
+                  </div><!-- /.fancybox-item -->
+                </div><!-- /.col-sm-6 -->
+                <div class="col-sm-6">
+                  <!-- fancybox item #3 -->
+                  <div class="fancybox-item d-flex">
+                    <div class="fancybox__icon">
+                      <i class="icon-diploma"></i>
+                    </div><!-- /.fancybox__icon -->
+                    <div class="fancybox__content">
+                      <h4 class="fancybox__title">Robert L. Noble Prize</h4>
+                      <p class="fancybox__desc">Canadian Cancer Society</p>
+                    </div><!-- /.fancybox-content -->
+                  </div><!-- /.fancybox-item -->
+                </div><!-- /.col-sm-6 -->
+                <div class="col-sm-6">
+                  <!-- fancybox item #4 -->
+                  <div class="fancybox-item d-flex">
+                    <div class="fancybox__icon">
+                      <i class="icon-diploma"></i>
+                    </div><!-- /.fancybox__icon -->
+                    <div class="fancybox__content">
+                      <h4 class="fancybox__title">National Prize for Medicine</h4>
+                      <p class="fancybox__desc">Chilean Academy of Medicine etc.</p>
+                    </div><!-- /.fancybox-content -->
+                  </div><!-- /.fancybox-item -->
+                </div><!-- /.col-sm-6 -->
+              </div><!-- /.row -->
+            </div><!-- /.fancybox-layout2 -->
+            
+        </div><!-- /.row -->
+      </div><!-- /.container -->
+    </section><!-- /.FAQ -->
+
+    
+  <button id="scrollTopBtn"><i class="fas fa-long-arrow-alt-up"></i></button>
+  </div><!-- /.wrapper -->
+
+  <script src="assets/js/jquery-3.5.1.min.js"></script>
+  <script src="assets/js/plugins.js"></script>
+  <script src="assets/js/main.js"></script>   
+
+    <!-- End of Footer -->
+
+    <!-- Footer Linked -->
+    <?php include('footer.inc'); ?>
+    <!-- Footer Linked -->   
+</body>
+</html>

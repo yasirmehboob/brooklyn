25bba61256cc90c89781fd0cdbc0f09c36d8e855
BigBook Cloud <yasir_u@yahoo.com>
2023-05-02 19:16:43 +0500

    Update get-quote.php

--- get-quote.php
+++ get-quote.php
@@ -13,6 +13,12 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
   <link rel="stylesheet" href="assets/css/libraries.css">
   <link rel="stylesheet" href="assets/css/style.css">
+    <style type="text/css">
+    
+        .contact-layout1 .contact-panel__info:after, .contact-layout2 .contact-panel__info:after{
+            width: 0px !important;
+        }
+    </style>
 </head>
 
 <body>
@@ -35,17 +41,18 @@
       <div class="container">
         <div class="row">
           <div class="col-12">
-            <div class="contact-panel d-flex flex-wrap">
-                
-                
-              <form class="contact-panel__form" method="post" action="assets/php/contact.php" id="quoteForm">
-                <div class="row">
-                  <div class="col-sm-12">
+            <div class="col-sm-12">
                     <h4 class="contact-panel__title">Get a Quote? </h4>
                     <p class="contact-panel__desc mb-30">Please feel welcome to contact our friendly reception staff
                       with any general enquiry.
                     </p>
                   </div>
+              
+            <div class="contact-panel d-flex flex-wrap">
+                
+                
+              <form class="contact-panel__form" method="post" action="assets/php/contact.php" id="quoteForm">
+                <div class="row">
                   <div class="col-sm-6 col-md-6 col-lg-6">
                     <div class="form-group">
                       <i class="icon-user form-group-icon"></i>
@@ -90,6 +97,44 @@
                   </div><!-- /.col-lg-12 -->
                 </div><!-- /.row -->
               </form>
+                
+                
+            <div class="contact-panel__info flex-column" id="accordion" style="padding:55px 20px;">
+                <h4 class="contact-panel__title">Quote FAQs</h4>
+            <div class="accordion-item opened">
+              <div class="accordion__header" data-toggle="collapse" data-target="#collapse3">
+                <a class="accordion__title" href="#">Who can get a Quote?</a>
+              </div><!-- /.accordion-item-header -->
+              <div id="collapse3" class="collapse show" data-parent="#accordion">
+                <div class="accordion__body">
+                  <p>Following Payment Methods are Available :<br>1. Credit / Debit Card<br>2. Easypaisa / JazzCash<br>3. Internet Banking<br>4. Cash on Delivery</p>
+                </div><!-- /.accordion-item-body -->
+              </div>
+            </div><!-- /.accordion-item -->
+            <div class="accordion-item">
+              <div class="accordion__header" data-toggle="collapse" data-target="#collapse1">
+                <a class="accordion__title" href="#">What is your response time?</a>
+              </div><!-- /.accordion-item-header -->
+              <div id="collapse1" class="collapse" data-parent="#accordion">
+                <div class="accordion__body">
+                  <p>With any financial product that you buy, it is important that you know you are getting the best
+                    advice from a reputable company as often</p>
+                </div><!-- /.accordion-item-body -->
+              </div>
+            </div><!-- /.accordion-item -->
+            <div class="accordion-item">
+              <div class="accordion__header" data-toggle="collapse" data-target="#collapse2">
+                <a class="accordion__title" href="#">How can i get Urgent assistance?</a>
+              </div><!-- /.accordion-item-header -->
+              <div id="collapse2" class="collapse" data-parent="#accordion">
+                <div class="accordion__body">
+                  <p>Yes ! Consultation is freely Available , You can Contact on the Given Numbers or leave a message on Whatsapp</p>
+                </div><!-- /.accordion-item-body -->
+              </div>
+            </div><!-- /.accordion-item -->
+            
+          
+            </div>
               
             </div>
           </div><!-- /.col-lg-6 -->
